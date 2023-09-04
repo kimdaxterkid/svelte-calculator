74404973182dec3de5f4f35bd6e305e3cab96a49
David Jin <kimdaxter@gmail.com>
2023-09-03 22:58:32 -0700

    initiate the svelte app
    - adding styles
    - adding html structure
    - adding basic scriptes

---
+++ src/routes/+page.svelte
@@ -0,0 +1,82 @@
+<script lang="ts">
+	const numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "."];
+	const operations = ["/", "x", "-", "+", "="];
+</script>
+
+<main>
+	<div class="calculator">
+		<div class="results" />
+		<div class="digits">
+			<div class="numbers">
+				<button class="btn btn-xlg"> C </button>
+				{#each numbers as number (number)}
+					<button class={`btn ${number === "0" ? "btn-lg" : null}`}> {number} </button>
+				{/each}
+			</div>
+			<div class="operations">
+				{#each operations as operation (operation)}
+					<button class="btn btn-orange"> {operation} </button>
+				{/each}
+			</div>
+		</div>
+	</div>
+</main>
+
+<style>
+	main {
+		width: 100vw;
+		height: 100vh;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	}
+	.calculator {
+		background-color: rgb(28, 28, 28);
+		width: 240px;
+		padding: 15px;
+		border-radius: 7px;
+	}
+	.digits {
+		display: flex;
+	}
+	.operations {
+		display: flex;
+		flex-direction: column;
+	}
+	.numbers {
+		display: flex;
+		flex-wrap: wrap;
+		width: 200px;
+	}
+	.btn {
+		width: 50px;
+		height: 50px;
+		border-radius: 100px;
+		background-color: rgb(114, 113, 113);
+		font-size: 20px;
+		font-weight: bold;
+		color: white;
+		margin: 5px;
+		border: none;
+	}
+	.btn-lg {
+		width: 110px;
+	}
+	.btn-orange {
+		background-color: orange;
+	}
+	.btn-silver {
+		background-color: silver;
+	}
+	.btn-xlg {
+		width: 170px;
+	}
+	.results {
+		height: 50px;
+		color: white;
+		font-size: 50px;
+		display: flex;
+		flex-direction: row-reverse;
+		margin-right: 10px;
+	}
+</style>
